c57ccc489a6c408a55452e1648daf3e09a80727d
Clovis <tsclovis@yahoo.fr>
2024-10-15 19:50:04 -0500

    recover old files

--- modules/terraform-helm-chart/external-dns/external-dns-iam-policy-and-role.tf
+++ modules/terraform-helm-chart/external-dns/external-dns-iam-policy-and-role.tf
@@ -1,28 +1,28 @@
-resource "aws_iam_role" "external-dns" {
-  name = "${data.aws_eks_cluster.example.name}-external-dns-iam-role"
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = "sts:AssumeRoleWithWebIdentity"
-        Effect = "Allow"
-        Sid    = ""
-        Principal = {
-          Federated = join("/", ["arn:aws:iam::${data.aws_caller_identity.current.account_id}:oidc-provider", split("https://", data.aws_eks_cluster.example.identity.0.oidc.0.issuer)[1]])
-        }
-        Condition = {
-          StringEquals = {
-            join(":", [split("https://", data.aws_eks_cluster.example.identity.0.oidc.0.issuer)[1], "sub"]) : "system:serviceaccount:${var.external-dns-ns}:${var.external-dns-sa}"
-          }
-        }
-      },
-    ]
-  })
+# resource "aws_iam_role" "external-dns" {
+#   name = "${data.aws_eks_cluster.example.name}-external-dns-iam-role"
+#   assume_role_policy = jsonencode({
+#     Version = "2012-10-17"
+#     Statement = [
+#       {
+#         Action = "sts:AssumeRoleWithWebIdentity"
+#         Effect = "Allow"
+#         Sid    = ""
+#         Principal = {
+#           Federated = join("/", ["arn:aws:iam::${data.aws_caller_identity.current.account_id}:oidc-provider", split("https://", data.aws_eks_cluster.example.identity.0.oidc.0.issuer)[1]])
+#         }
+#         Condition = {
+#           StringEquals = {
+#             join(":", [split("https://", data.aws_eks_cluster.example.identity.0.oidc.0.issuer)[1], "sub"]) : "system:serviceaccount:${var.external-dns-ns}:${var.external-dns-sa}"
+#           }
+#         }
+#       },
+#     ]
+#   })
 
-  tags = {
-    tag-key = "${data.aws_eks_cluster.example.id}-external-dns-iam-role"
-  }
-}
+#   tags = {
+#     tag-key = "${data.aws_eks_cluster.example.id}-external-dns-iam-role"
+#   }
+# }
 
 resource "aws_iam_policy" "external-dns-polcy" {
   name        = "external-dns-polcy"
@@ -57,7 +57,36 @@ resource "aws_iam_policy" "external-dns-polcy" {
 }
 
 resource "aws_iam_role_policy_attachment" "external-dns_attach" {
-  role       = aws_iam_role.external-dns.name
+  role       = aws_iam_role.external_dns.name
   policy_arn = aws_iam_policy.external-dns-polcy.arn
 }
 
+
+
+resource "aws_iam_role" "external_dns" {
+  name = "${data.aws_eks_cluster.example.name}-external-dns-iam-role"
+  
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"  # Open assume_role_policy
+    Statement = [  # Open Statement array
+      {
+        Action = "sts:AssumeRoleWithWebIdentity"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {  # Open Principal object
+          Federated = join("/", ["arn:aws:iam::${data.aws_caller_identity.current.account_id}:oidc-provider", split("https://", data.aws_eks_cluster.example.identity.0.oidc.0.issuer)[1]])
+        },  # Close Principal object
+        Condition = {  # Open Condition object
+          StringEquals = {  # Open StringEquals object
+            "${split("https://", data.aws_eks_cluster.example.identity.0.oidc.0.issuer)[1]}:sub": "system:serviceaccount:external-dns:external-dns-sa"
+          }  # Close StringEquals object
+        }  # Close Condition object
+      }  # Close Statement object
+    ]  # Close Statement array
+  })  # Close assume_role_policy
+  
+  tags = {  # Open tags object
+    Name = "${data.aws_eks_cluster.example.name}-external-dns-iam-role"
+    Environment = "production"  # You can add more specific tags based on your use case
+  }  # Close tags object
+}
--- modules/terraform-helm-chart/external-dns/main.tf
+++ modules/terraform-helm-chart/external-dns/main.tf
@@ -1,16 +1,16 @@
 # Resource: Helm Release 
 resource "helm_release" "external_dns" {
-  depends_on = [aws_iam_role.external-dns]
-  name       = var.external-dns-ns
+  # depends_on = [aws_iam_role.external-dns]
+  name       = "external-dns"   #var.external-dns-ns
 
   repository = "https://kubernetes-sigs.github.io/external-dns/"
   chart      = "external-dns"
 
-  namespace = var.external-dns-ns
+  namespace = "external-dns"  #var.external-dns-ns
 
   set {
     name  = "image.repository"
-    value = "k8s.gcr.io/external-dns/external-dns"
+    value = "registry.k8s.io/external-dns/external-dns"    #"k8s.gcr.io/external-dns/external-dns"
   }
 
   set {
@@ -20,12 +20,12 @@ resource "helm_release" "external_dns" {
 
   set {
     name  = "serviceAccount.name"
-    value = var.external-dns-sa
+    value = "external-dns-sa"   #var.external-dns-sa
   }
 
   set {
     name  = "serviceAccount.annotations.eks\\.amazonaws\\.com/role-arn"
-    value = aws_iam_role.external-dns.arn
+    value = "arn:aws:iam::410052665018:role/2526-dev-alpha-external-dns-iam-role"    #aws_iam_role.external-dns.arn
   }
 
   set {
